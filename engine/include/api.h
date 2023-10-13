13b513137830d1d8981c799b0eb86c4db17d7a45
jaroslav2324 <jaro_r@mail.ru>
2023-10-13 18:44:10 +0300

    Built with .so under Linux

--- engine/include/api.h
+++ engine/include/api.h
@@ -1,5 +1,9 @@
 #pragma once
 
-#define EXPORTLIB __declspec(dllexport)
+#ifdef _WIN32
+    #define EXPORTLIB __declspec(dllexport)
+#elif __linux__
+    #define EXPORTLIB __attribute__((visibility("default")))
+#endif
 
 EXPORTLIB int test_sum(int a, int b);
