50633fe2490caf9775f99d7c1a7f7d588ab4ad51
jaroslav2324 <jaro_r@mail.ru>
2023-12-30 14:39:27 +0300

    removed comments from test file

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,52 +1,10 @@
 
 #include "../../engine/include/eng/engine.hpp"
 
-
-
-
-// class HelloTriangleApplication {
-
-// private:
-
-
-//     bool checkValidationLayerSupport() {
-
-//         uint32_t layerCount;
-//         vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
-
-//         std::vector<VkLayerProperties> availableLayers(layerCount);
-//         vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
-
-//         for (const char* layerName : validationLayers) {
-//             bool layerFound = false;
-
-//             for (const auto& layerProperties : availableLayers) {
-//                 if (strcmp(layerName, layerProperties.layerName) == 0) {
-//                     layerFound = true;
-//                     break;
-//                 }
-//             }
-
-//             if (!layerFound) {
-//                 return false;
-//             }
-//     }
-
-//     return true;
-// }
-
-// };
-
-
-
 int main() {
-
-    //std::cout << test_sum(1, 2) << std::endl;
-    //std::cout << test_min(1, 2) << std::endl;
 
     Engine eng;
     eng.startup();
-    std::cout << "Starting loop\n";
     eng.loop();
     eng.shutdown();
 
